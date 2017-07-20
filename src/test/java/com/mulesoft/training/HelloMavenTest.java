f6dd8a2735890ba248cc6a213bab75cfa45ac8c3
David Bauer <david@Davids-New-MacBook-Pro.local>
2017-07-20 09:32:32 -0500

    Adding new test that fails

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -22,6 +23,14 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String[] getConfigFiles() {
         String files[] = {"maven-project.xml","global.xml"};
     	return files;
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\n\nDynamic Port in Test Case 1: " + dynamicPort.getNumber() + "\n\n");
+    	MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 
 }
